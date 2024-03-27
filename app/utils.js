75a5e795c4a0e1bb5fda579527d496fefa313b40
sandeep lakhiwal <sandeeplakhiwal98@gmail.com>
2024-03-27 18:52:00 +0530

    Changed server-deployment-server and server-url

--- app/utils.js
+++ app/utils.js
@@ -3,7 +3,8 @@ import { cache } from "react";
 
 export const getProduct = cache(async (id) => {
   // const server = "http://localhost:5000/api/v1";
-  const server = "https://habuli-server.onrender.com/api/v1";
+  const server = "https://habuli-backend-production.up.railway.app/api/v1";
+  // const server = "https://habuli-server.onrender.com/api/v1";
 
   try {
     const response = await axios.get(`${server}/product/${id}`, {
